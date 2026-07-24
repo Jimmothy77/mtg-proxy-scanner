564557a937cf997265460f35f58aefab593461a5
Jimmothy77 <jmrust77@gmail.com>
2026-07-24 12:49:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -54,12 +54,8 @@ if picture:
     else:
       st.error(f"Couldn't match card text: '{extracted_text}'")
 
-# Use this if you want a visual line in the app!
+# Visual divider
 st.divider()
-
-# Display Active Decklist
-st.subheader("📋 Current Decklist")
-if st.session_state.decklist:
 
 # Display Active Decklist
 st.subheader("📋 Current Decklist")
